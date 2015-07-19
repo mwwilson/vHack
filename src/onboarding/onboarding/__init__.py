51b2ff09cda6ff5344c40d23f38ea652217db800
Marshall Wilson <marshall@marshallsonsmbp.hq.corp.viasat.com>
2015-07-19 02:33:39 -0400

    Added login

--- src/onboarding/onboarding/__init__.py
+++ src/onboarding/onboarding/__init__.py
@@ -39,6 +39,7 @@ def main(global_config, **settings):
     authn_policy = AuthTktAuthenticationPolicy('soseekrit')
     authz_policy = ACLAuthorizationPolicy()
 
+    settings['users'] = {}
     config = Configurator(settings=settings,
         root_factory=root_factory,
         authentication_policy=authn_policy,
@@ -48,6 +49,8 @@ def main(global_config, **settings):
     config.add_route('home', '/')
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
+    config.add_route('signup', '/signup')
+    config.add_route('login_success', '/login_success')
     config.add_route('application', '/application')
     config.scan()
     return config.make_wsgi_app()
--- src/onboarding/onboarding/views.py
+++ src/onboarding/onboarding/views.py
@@ -2,6 +2,14 @@ from pyramid.security import Allow, Authenticated, remember, forget
 from pyramid.view import view_config
 from pyramid.response import Response
 
+def verify(users, creds):
+    try:
+        if creds['username'] in users:
+            if creds['password'] == users[creds['username']]['password']:
+                return creds['username']
+    except:
+        pass
+    return None
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt',
              permission='view')
@@ -12,15 +20,34 @@ def my_view(request):
 def application(request):
     return {'project': 'onboarding'}
 
-@view_config(route_name='login')
+@view_config(route_name='login', renderer='templates/login.pt')
 def login(request):
-    #userid = request.params.get('userid')
-    userid = 'Bob'
+    return {'login' : 'page'}
+
+@view_config(route_name='signup')
+def signup(request):
+    creds = request.json_body
+    users = request.registry._get_settings()['users']
+    users[creds['username']] = {'password':creds['password']}
+    userid = creds['username']
     headers = remember(request, userid)
     return Response(
-        'Logged in as %s' % userid,
+        'Account for user %s created' % userid,
         headers=headers
         )
+
+@view_config(route_name='login_success')
+def login_success(request):
+    creds = request.json_body
+    userid = verify(request.registry._get_settings()['users'], creds)
+    if userid:
+        headers = remember(request, userid)
+        return Response(
+            'Logged in as %s' % userid,
+            headers=headers
+            )
+    else:
+        return Response('Login attempt failed')
 
 @view_config(route_name='logout')
 def logout(request):
