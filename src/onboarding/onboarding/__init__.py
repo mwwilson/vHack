b2de75ccbadb9a6818f29bf0e2811684d9616c5b
Marshall Wilson <marshall@marshallsonsmbp.hq.corp.viasat.com>
2015-07-18 21:47:40 -0400

    added auth

--- src/onboarding/onboarding/__init__.py
+++ src/onboarding/onboarding/__init__.py
@@ -1,13 +1,53 @@
+from pyramid.response import Response
 from pyramid.config import Configurator
+from pyramid.security import Allow, Authenticated, remember, forget
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
 
+class Resource(object):
+    def __init__(self, name='', acl=None,
+                 parent=None):
+        self.children = {}
+        self.__name__ = name
+        self.__parent__ = parent
+        if acl is not None:
+            self.__acl__ = acl
+
+    def add_subresource(self, name, acl=None):
+        self.children[name] = Resource(
+            name, acl=acl, parent=self
+            )
+
+    def __getitem__(self, name):
+        return self.children[name]
+
+    def __repr__(self):
+        return '<Resource named %r at %s>' % (
+            self.__name__, id(self)
+            )
+
+def root_factory(request):
+    root = Resource(
+        '',
+        acl=[(Allow, Authenticated, 'view')]
+        )
+    return root
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    authn_policy = AuthTktAuthenticationPolicy('soseekrit')
+    authz_policy = ACLAuthorizationPolicy()
+
+    config = Configurator(settings=settings,
+        root_factory=root_factory,
+        authentication_policy=authn_policy,
+        authorization_policy=authz_policy)
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=0)
     config.add_route('home', '/')
+    config.add_route('login', '/login')
+    config.add_route('logout', '/logout')
     config.add_route('application', '/application')
     config.scan()
     return config.make_wsgi_app()
--- src/onboarding/onboarding/views.py
+++ src/onboarding/onboarding/views.py
@@ -1,10 +1,31 @@
+from pyramid.security import Allow, Authenticated, remember, forget
 from pyramid.view import view_config
+from pyramid.response import Response
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/mytemplate.pt',
+             permission='view')
 def my_view(request):
     return {'project': 'onboarding'}
 
 @view_config(route_name='application', renderer='templates/application.pt')
 def application(request):
     return {'project': 'onboarding'}
+
+@view_config(route_name='login')
+def login(request):
+    #userid = request.params.get('userid')
+    userid = 'Bob'
+    headers = remember(request, userid)
+    return Response(
+        'Logged in as %s' % userid,
+        headers=headers
+        )
+
+@view_config(route_name='logout')
+def logout(request):
+    headers = forget(request)
+    return Response(
+        'Logged out',
+        headers=headers
+        )
